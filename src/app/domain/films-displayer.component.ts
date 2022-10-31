2979da5c1e5e4b5945c540df8aaa6f59bd88210c
ddl <ddl@novatec-gmbh.de>
2022-10-31 13:17:11 +0100

    feat: Define ports first approach

--- src/app/domain/films-displayer.component.ts
+++ src/app/domain/films-displayer.component.ts
@@ -1,10 +1,29 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import PDisplayFilms from '../ports/p-display-films';
+import { Film } from './models/film';
 
 @Injectable({
   providedIn: 'root'
 })
-export class FilmsDisplayerComponent {
+export class FilmsDisplayerComponent implements PDisplayFilms {
+  
+  films: Film[] = [];
+  filter: string = '';
 
   constructor() { }
+
+  askFilmsList(): Observable<void> {
+    throw new Error('Method not implemented.');
+  }
+  askFilmsFiltered(filter: string, allowEmpty?: boolean | undefined): Observable<void> {
+    throw new Error('Method not implemented.');
+  }
+  askFilmCreation(filmName: string): Observable<void> {
+    throw new Error('Method not implemented.');
+  }
+  askFilmDeletion(film: Film): Observable<void> {
+    throw new Error('Method not implemented.');
+  }
 
 }
--- src/app/domain/models/film.ts
+++ src/app/domain/models/film.ts
@@ -1,4 +1,4 @@
-export interface Hero {
+export interface Film {
     id: number;
     name: string;
     description: string;
